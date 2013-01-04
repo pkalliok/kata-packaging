7c92c811eda59db2559246c2ea409a6abf9d7375
Pinja Koskinen <pinja.koskinen@csc.fi>
2013-01-04 11:25:40 +0200

    Upgrading Solr to version 4.0.0

--- rpms/common/22configsolr.sh
+++ rpms/common/22configsolr.sh
@@ -6,7 +6,7 @@ then
   exit 0
 fi
 instloc=$1
-cp /opt/solr/conf/schema.xml /opt/solr/conf/schema.xml.bak
-cp $instloc/pyenv/src/ckan/ckan/config/solr/schema-2.0.xml /opt/solr/conf/schema.xml
+cp /opt/data/solr/collection1/conf/schema.xml /opt/data/solr/collection1/conf/schema.xml.bak
+cp $instloc/pyenv/src/ckan/ckan/config/solr/schema-2.0.xml /opt/data/solr/collection1/conf/schema.xml
 service tomcat6 restart
 chkconfig tomcat6 on
