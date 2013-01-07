9b6c37ce9a3dc68d29525369f68223b0213981ab
Pinja Koskinen <pinja.koskinen@csc.fi>
2013-01-07 07:56:19 +0200

    Upgraded Solr to 4.0.0 for autobuild and fixed some references of Solr to the new version in README

--- autobuild/12buildinstallsolr.sh
+++ autobuild/12buildinstallsolr.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 cd rpmbuild/SOURCES
-curl -O http://archive.apache.org/dist/lucene/solr/3.5.0/apache-solr-3.5.0.tgz
+curl -O http://archive.apache.org/dist/lucene/solr/4.0.0/apache-solr-4.0.0.tgz
 cd ../SPECS
 ln -s ../../rpms/solr/solr.spec
 rpmbuild -ba solr.spec
 arch=$(rpm -q --qf '%{arch}\n' --specfile solr.spec | head -n 1)
 cd ../RPMS/${arch}
-sudo yum install -y apache-solr-3.5.0-1.el6.${arch}.rpm
+sudo yum install -y apache-solr-4.0.0-1.el6.${arch}.rpm
